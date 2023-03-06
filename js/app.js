e67d08f91faa2a51edc435cebfe6de536b73a815
lp-deadpool0 <lpdead@gmail.com>
2023-03-06 23:05:03 +0300

    some fixes

--- js/app.js
+++ js/app.js
@@ -8,22 +8,3 @@ navigator.mediaDevices
   .catch(function (err) {
     console.log("Error accessing camera: " + err);
   });
-
-let button = document.querySelector("button");
-
-button.addEventListener("click", (ev) => {
-  let rect = button.getBoundingClientRect();
-  let print = document.createElement("span");
-  print.className = "button__print";
-
-  print.style.left = ev.pageX - button.offsetLeft + "px";
-  print.style.top = ev.pageY - button.offsetTop + "px";
-
-  button.appendChild(print);
-
-  console.log(
-    ` + ${ev.clientX - rect.left}:${ev.clientY - rect.top}    ----------    ${
-      ev.pageX - button.offsetLeft
-    }:${ev.pageY - button.offsetTop}`
-  );
-});
