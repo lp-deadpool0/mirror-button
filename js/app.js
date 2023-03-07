e67e2956493063658ccee8bd7e59f530c8b0722d
lp-deadpool0 <lpdead@gmail.com>
2023-03-07 20:17:50 +0300

    mirror button v2

--- js/app.js
+++ js/app.js
@@ -3,8 +3,26 @@ navigator.mediaDevices
   .then(function (stream) {
     var video = document.querySelector(".video");
     video.srcObject = stream;
+    video.playbackRate = 0.1;
     video.play();
   })
   .catch(function (err) {
     console.log("Error accessing camera: " + err);
   });
+
+const button = document.querySelector(".button");
+const dirtyTexture = document.querySelector(".button__texture");
+const counterView = document.querySelector(".counter");
+let clickCounter = 0;
+
+button.addEventListener("click", () => {
+  var audio = new Audio();
+  audio.volume = 0.4;
+  audio.src = "../audio/click.mp3";
+  audio.autoplay = true;
+
+  dirtyTexture.style.opacity = clickCounter / 100;
+  counterView.innerText = clickCounter;
+
+  clickCounter++;
+});
